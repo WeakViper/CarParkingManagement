c120c9d5b3586df4d31c16c3eae09a672e056f2b
UBC Student <akhan42@students.cs.ubc.ca>
2024-04-02 19:13:37 -0700

    Updated Exit logic and enter logic to handle reduncancy

--- backend/routes/EnterExit.js
+++ backend/routes/EnterExit.js
@@ -24,38 +24,77 @@ router.post('/enter', async (req, res) => {
     const sql1 = 'INSERT INTO VehicleClass (plateNumber, weightClass) VALUES (?, ?)';
     const values1 = [plateNumber, weightClass];
 
-    // this is to insert the vehicle into the VehicleClass table
-    db.query(sql1, values1, (err, result, fields) => {
-        if (err) {
-            return res.send(err.message);
-        }
-        return console.log(result);
-    })
-    let sql2;
-    let values2;
-    if (weightClass === "A") {
-        sql2 = 'INSERT INTO PrivateCar VALUES (?, ?)';
-        values2 = [plateNumber, extra];
-    } else if (weightClass === "B") {
-        sql2 = 'INSERT INTO HeavyDuty VALUES (?, ?)';
-        values2 = [plateNumber, extra];
-    } else if (weightClass === "C") {
-        sql2 = 'INSERT INTO Maintenance VALUES (?, ?)';
-        values2 = [plateNumber, extra];
-    } else {
-        sql2 = 'INSERT INTO PrivateCar VALUES (?, ?)';
-        values2 = [plateNumber, extra]; //remeber to get right extra val from frontend
-        console.log("Invalid weight class");
+    function checkPlateNumber (plateNumber) {
+        const query = 'SELECT * FROM Occupy WHERE PlateNumber = ?';
+    
+        db.query(query, [plateNumber], (err, results) => {
+            if (err) {
+                console.error('Error executing query:', err);
+                return;
+            }
+    
+            if (results.length > 0) {
+                console.log(`Plate number ${plateNumber} exists in the Occupy table.`);
+                db.query(sql1, values1, (err, result, fields) => {
+                    if (err) {
+                        return res.send(err.message);
+                    }
+                    return console.log(result);
+                })
+                let sql2;
+                let values2;
+                if (weightClass === "A") {
+                    sql2 = 'INSERT INTO PrivateCar VALUES (?, ?)';
+                    values2 = [plateNumber, extra];
+                } else if (weightClass === "B") {
+                    sql2 = 'INSERT INTO HeavyDuty VALUES (?, ?)';
+                    values2 = [plateNumber, extra];
+                } else if (weightClass === "C") {
+                    sql2 = 'INSERT INTO Maintenance VALUES (?, ?)';
+                    values2 = [plateNumber, extra];
+                } else {
+                    sql2 = 'INSERT INTO PrivateCar VALUES (?, ?)';
+                    values2 = [plateNumber, extra]; //remeber to get right extra val from frontend
+                    console.log("Invalid weight class");
+                }
+            
+                // this is to insert the vehicle into the correct table
+                db.query(sql2, values2, (err, result, fields) => {
+                    if (err) {
+                        console.log(err.message);
+                        return;
+                    }
+                    return console.log(result);
+                })
+            } else {
+                console.log(`Plate number ${plateNumber} does not exist in the Occupy table.`);
+                let sql2;
+                const values2 = [extra, plateNumber];
+                if (weightClass === "A") {
+                    sql2 = 'UPDATE PrivateCar SET Extra = ? WHERE PlateNumber = ?';
+                } else if (weightClass === "B") {
+                    sql2 = 'UPDATE HeavyDuty SET Extra = ? WHERE PlateNumber = ?';
+                } else if (weightClass === "C") {
+                    sql2 = 'UPDATE Maintenance SET Extra = ? WHERE PlateNumber = ?';
+                } else {
+                    sql2 = 'UPDATE PrivateCar SET Extra = ? WHERE PlateNumber = ?';
+                    console.log("Invalid weight class");
+                }
+            
+                // this is to insert the vehicle into the correct table
+                db.query(sql2, values2, (err, result, fields) => {
+                    if (err) {
+                        console.log(err.message);
+                        return;
+                    }
+                    return console.log(result);
+                })
+            }
+        });
     }
-
-    // this is to insert the vehicle into the correct table
-    db.query(sql2, values2, (err, result, fields) => {
-        if (err) {
-            console.log(err.message);
-            return;
-        }
-        return console.log(result);
-    })
+    
+    // Example usage
+    checkPlateNumber(plateNumber);
     
     let sql3 = 'INSERT INTO Enters VALUES (NOW(), ?, ?)';
     let values3 = [entryGate, plateNumber];   
@@ -163,11 +202,144 @@ router.post('/enter', async (req, res) => {
         return console.log(result);
     })
 
-    console.log("C");
-
     res.send("Vehicle entered successfully!");
 })
 
 
+
+router.post('/exit', async (req, res) => {
+    const { plateNumber, exitGate, method } = req.body;
+
+    const getSlot = async (plateNumber) => {
+        return new Promise((resolve, reject) => {
+            db.query("SELECT ParkingSlotID FROM Occupy WHERE PlateNumber = ? LIMIT 1", [plateNumber], (err, result, fields) => {
+                if (err) {
+                    reject(err.message);
+                } else {
+                    console.log(result);
+                    resolve(result[0].ParkingSlotID);
+                }
+            })
+        });
+    }
+
+    // Usage
+    const slotID = await getSlot(plateNumber);
+
+    const getZone = async (plateNumber) => {
+        return new Promise((resolve, reject) => {
+            db.query("SELECT ParkingZoneID FROM Occupy WHERE PlateNumber = ? LIMIT 1", [plateNumber], (err, result, fields) => {
+                if (err) {
+                    reject(err.message);
+                } else {
+                    resolve(result[0].ParkingZoneID);
+                }
+            })
+        });
+    }
+
+    // Usage
+    const zoneID = await getZone(plateNumber);
+
+    const enterIntoExits = async (plateNumber) => {
+        return new Promise((resolve, reject) => {
+            db.query("INSERT INTO Exits VALUES (NOW(),?, ?, ?, ?)", [exitGate, plateNumber, zoneID, slotID], (err, result, fields) => {
+                if (err) {
+                    reject(err.message);
+                } else {
+                    resolve();
+                }
+            })
+        });
+    }
+
+    // Usage
+    await enterIntoExits(plateNumber);
+
+    const sql2 = 'DELETE FROM Occupy WHERE plateNumber = ?';
+    const values2 = [plateNumber];
+
+    // this is to delete the vehicle from the Occupy table
+    db.query(sql2, values2, (err, result, fields) => {
+        if (err) {
+            return res.send(err.message);
+        }
+        return console.log(result);
+    })
+
+    const getClass = async (plateNumber) => {
+        return new Promise((resolve, reject) => {
+            db.query("SELECT WeightClass FROM VehicleClass WHERE PlateNumber = ?", [plateNumber], (err, result, fields) => {
+                if (err) {
+                    reject(err.message);
+                } else {
+                    resolve(result[0].WeightClass);
+                }
+            })
+        });
+    }
+
+    // Usage
+    const weightClass = await getClass(plateNumber);
+
+    const getRate = async (weightClass) => {
+        return new Promise((resolve, reject) => {
+            db.query("SELECT HourlyRate FROM WeightRate WHERE WeightClass = ?", [weightClass], (err, result, fields) => {
+                if (err) {
+                    reject(err.message);
+                } else {
+                    resolve(result[0].HourlyRate);
+                }
+            })
+        });
+    }
+
+    
+    // Usage
+    const weightRate = await getRate(weightClass);
+
+    const entryTimeQuery = `SELECT 
+    TIMESTAMPDIFF(
+        SECOND, 
+        (SELECT e.DateTime FROM Enters e WHERE e.PlateNumber = ? ORDER BY e.DateTime DESC LIMIT 1),
+        (SELECT e.DateTime FROM Exits e WHERE e.PlateNumber = ? ORDER BY e.DateTime DESC LIMIT 1)
+    ) / 3600.0 AS DifferenceInHours`
+
+    const getTimeDifference = async (plateNumber) => {
+        return new Promise((resolve, reject) => {
+            db.query(entryTimeQuery, [plateNumber, plateNumber], (err, result, fields) => {
+                if (err) {
+                    reject(err.message);
+                } else {
+                    resolve(result[0].DifferenceInHours);
+                }
+            })
+        });
+    }
+
+    const timeDifference = await getTimeDifference(plateNumber);
+
+    let amount = timeDifference * weightRate;
+
+    const randomId = Math.floor(Math.random() * 1000); // Generates a random number between 0 and 999
+    const paymentId = `PID${randomId}`;
+
+    // Add the generated payment ID to the beginning of the values array
+    const values3 = [paymentId, method, plateNumber, exitGate, amount];
+
+    const sql3 = 'INSERT INTO Payment VALUES (?, ?, ?, ?, ?)';
+
+    db.query(sql3, values3, (err, result, fields) => {
+        if (err) {
+            console.log(err.message);
+        } else {
+            console.log(result);
+        }
+    })
+
+    res.send("Exit successful!");
+
+
+})
 
 module.exports = router;
